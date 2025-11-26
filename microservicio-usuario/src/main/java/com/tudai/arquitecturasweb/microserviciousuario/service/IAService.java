01ef0090079a035aca08f15de9215d8658ca78a7
nachoti19 <116025391+nachoti19@users.noreply.github.com>
2025-11-25 21:25:29 -0300

    correccion client

--- microservicio-usuario/src/main/java/com/tudai/arquitecturasweb/microserviciousuario/service/IAService.java
+++ microservicio-usuario/src/main/java/com/tudai/arquitecturasweb/microserviciousuario/service/IAService.java
@@ -13,7 +13,9 @@ import org.springframework.transaction.annotation.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
